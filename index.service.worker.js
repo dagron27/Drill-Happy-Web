2f18ca195d3c7c164178353e02aff7f0b2c780e1
daniel-leone <123667871+dagron27@users.noreply.github.com>
2026-08-31 20:07:47 -0500

    deploy: update web build (debug export, 2026-09-01T01:07:46Z)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788224717|13035946';
+const CACHE_VERSION = '1788224862|13446622';
 /** @type {string} */
 const CACHE_PREFIX = 'Drill-Happy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
